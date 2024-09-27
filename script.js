9b027af1012c2b0d5bc72d76b56a4736ff3cfe52
isrealthekid <oluwoleisreal12@gmail.com>
2024-09-27 13:59:51 +0100

    mouseMoveAnimation

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 
 
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+//  THE PLAY BUTTON ANIMATION : MOVES ON MOUSE MOVE
+function videoconAnimation() {
+    var videocon = document.querySelector('#video-container')
+    var playbtn = document.querySelector('#play')
+    videocon.addEventListener("mouseenter",function(){
+    gsap.to(playbtn,{
+            scale: 1,
+            opacity: 1
+    })
+    
+    })
+
+    videocon.addEventListener("mouseleave",function(){
+        gsap.to(playbtn,{
+            scale: 0,
+            opacity:0
+        })
+    })
+
+    videocon.addEventListener("mousemove",function(dets){
+        gsap.to(playbtn,{
+            left:dets.x-2,
+            top:dets.y-1
+        })
+
+    })
+}
+
+videoconAnimation()
+// 
+
 function loadingAnimation(){
     gsap.from("#page1 h1 ",{
         y:100,
