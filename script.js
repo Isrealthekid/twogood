1be8f96b53850462b251f84964e4164a01a8a4f1
isrealthekid <oluwoleisreal12@gmail.com>
2024-09-28 13:17:09 +0100

    page2Cursor

--- script.js
+++ script.js
@@ -54,3 +54,28 @@ function loadingAnimation(){
 }
 
 loadingAnimation()
+
+
+
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+document.querySelectorAll('.child').forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to("#cursor",{
+            transform:'translate(-50%, -50%) scale(1)'
+        });
+    });
+    elem.addEventListener("mouseleave",function(){
+        gsap.to("#cursor",{
+            transform:'translate(-50%, -50%) scale(0)'
+        })
+    })
+})
+
+
